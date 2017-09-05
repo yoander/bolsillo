4aad570eca075a7c6014be3e0373ebd7af98ce50
Yoander Valdés Rodríguez <yoander.valdes@gmail.com>
2017-09-04 19:50:21 -0500

    Add tags for new transactions

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     $('.tag').click(function () {
         var tag = $(this);
+        var input = $('input#tags');
+        var tags = JSON.parse(input.val());
         if (tag.hasClass('badge-light')) {
+            tags.push(tag.attr('id'));
             tag.removeClass('badge-light').addClass('badge-info').html('✔ ' + tag.html());
         } else if (tag.hasClass('badge-info')) {
-            var html = 
+            console.log(tags.splice(tags.indexOf(tag.attr('id'), 1)));
             tag.removeClass('badge-info').addClass('badge-light').html(tag.html().replace('✔ ', ''));
         }
+        tags = input.val(JSON.stringify(tags));
     });
 });
 /*
