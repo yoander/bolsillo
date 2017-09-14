32e0cd1624a910a3818bd6d974a10b0a750f778d
Yoander Valdés Rodríguez <yoander.valdes@gmail.com>
2017-09-13 20:13:57 -0500

    Fix issue in setting tags in trasaction form

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     $('.tag').click(function () {
         var tag = $(this);
-        //var elem = { "id": tag.attr('id'), "tag": tag.text() };
         var id = tag.attr('id')
+        console.log("ID", id)
         var input = $('input#tags');
         var tags = JSON.parse(input.val());
-        if (tag.hasClass('badge-light')) {
+        if (tag.hasClass('badge-danger')) {
             tags.push(id);
-            tag.removeClass('badge-light').addClass('badge-info').html('✔' + tag.html());
+            tag.removeClass('badge-danger').addClass('badge-info').html('✔' + tag.html());
         } else if (tag.hasClass('badge-info')) {
-            tags.splice(tags.indexOf(id, 1));
-            tag.removeClass('badge-info').addClass('badge-light').html(tag.html().replace('✔', ''));
+            tags.splice(tags.indexOf(id), 1);
+            tag.removeClass('badge-info').addClass('badge-danger').html(tag.html().replace('✔', ''));
         }
-        tags = input.val(JSON.stringify(tags));
+        input.val(JSON.stringify(tags));
     });
 });
 /*
