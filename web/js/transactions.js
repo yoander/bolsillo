0e5aa9ce680dc7bacc7339480a819ae1bddfb560
Yoander Valdés Rodríguez <yoander.valdes@gmail.com>
2018-01-14 22:34:40 -0500

    Remove notification component from filtering transaction method

--- web/js/transactions.js
+++ web/js/transactions.js
@@ -1,27 +1,7 @@
 $(document).ready(function () {
     $('#filter').click(function() {
-        document.body.style.cursor='wait';
-        toastr.options = {
-            "closeButton": false,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-bottom-right",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "0",
-            "extendedTimeOut": "0",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
-
-        toastr.info("Loading transactions");
-
         var filter = $(this);
+        var faIcon = $(this).children('i');
         $.ajax({
             url: $(this).attr('data-url'),
             data: {
@@ -31,15 +11,16 @@ $(document).ready(function () {
             },
             beforeSend: function() {
                 filter.prop('disabled', true);
+                faIcon.removeClass('fa-filter')
+                    .addClass('fa-spinner fa-spin fa-fw');    
             },
             success: function(data) {
-                toastr.success("Transactions loaded");
                 $('#transTable').html(data);
             },
             complete: function () {
-                window.setTimeout(function() { toastr.clear(); }, 3000)
+                faIcon.removeClass('fa-spinner fa-spin fa-fw')
+                    .addClass('fa-filter');
                 filter.prop('disabled', false);
-                document.body.style.cursor='default';
             }
         });
     }) ;
